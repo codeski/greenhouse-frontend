f0dda43782fdd2a96c15efb6b3b976380840709d
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-06-24 18:09:59 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import plantsReducer from './reducers/plantsReducer.js'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({ plants: plantsReducer })
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +25,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/reducers/plantsReducer.js
@@ -0,0 +1,8 @@
+export default function plantsReducer(state = [], action) {
+    switch(action.type){
+      case 'ADD_PLANT':
+        return state
+      default: 
+          return state
+    }
+}
