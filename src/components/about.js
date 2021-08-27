e0366a7cf47e50e46653c95e50b6ed007dcb4f48
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-08-27 13:51:00 -0600

    created a project for timers made for final blogs and deleted them from this project to simplify code

--- src/components/about.js
+++ src/components/about.js
@@ -3,16 +3,10 @@ import Title from './title'
 import Description from './description'
 import Image from 'react-bootstrap/Image'
 import plantImage from '../assets/plantImage.jpg'
-// import HooksTimer from './hooksTimer'
-// import Counter from './hooksTimer'
-// import Timer from './timer'
 
 const About = () => {
     return (
         <div className="home">
-            {/* <Timer /> */}
-            {/* <Counter /> */}
-            {/* <HooksTimer /> */}
             <Title />
             <Description />
             <Image src={plantImage} fluid />
